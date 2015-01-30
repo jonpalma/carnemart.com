5f231b67ca9e787d721d3a14969a2088730836e6
Lázaro Reyes <lazaro.reyesfierro@gmail.com>
2015-01-30 12:53:42 -0700

    inicio del html estático

---
+++ web/index.php
@@ -0,0 +1,125 @@
+<?php include('modulos/header.php'); ?>
+           <div class="jumbotron">
+              <!-- SLIDE -->
+               <a href="#"><img src="img/banner/1.jpg" alt="1" class="img-responsive"></a>
+              <!-- END SLIDE -->
+           </div>
+           <div class="container">
+              <div class="row">
+                 <span class="separator"></span>
+                 <h2>Concepto Integral</h2>
+                 <span class="separator"></span>
+                 <div class="col-md-4">
+                     <img src="img/negocio.png" alt="negocio" class="img-responsive">
+                     <p>Este es un texto que describe al icono de arriba Todo para tu Negocio de CarneMart</p>
+                     <a href="#" class="btn-promo">Ver Promociones</a>
+                 </div>
+                 <div class="col-md-4">
+                     <img src="img/reunion.png" alt="negocio" class="img-responsive">
+                     <p>Este es un texto que describe al icono de arriba Reunión Especial de CarneMart</p>
+                     <a href="#" class="btn-promo">Ver Promociones</a>
+                 </div>
+                 <div class="col-md-4">
+                     <img src="img/cocina.png" alt="negocio" class="img-responsive">
+                     <p>Este es un texto que describe al icono de arriba Cocina Diaria de CarneMart</p>
+                     <a href="#" class="btn-promo">Ver Promociones</a>
+                 </div>
+              </div>
+           </div>
+           <div class="row">
+               <img src="img/fondo.jpg" alt="CarneMart Compañía" class="img-responsive">
+               <div class="container">
+                   <h3>Carne Mart</h3>
+               <blockquote>
+                   "Somos una solución integral para negocios institucionales de alimentos y mayoristas, ofrecemos un alto nivel de servicio y un amplio surtido de productos de calidad al mejor precio de mercado".
+               </blockquote>
+               </div>
+           </div>
+           <div class="container">
+              <div class="row">
+                  <div class="container">
+                   <span class="separator"></span>
+                   <h2>Productos</h2>
+                   <span class="separator"></span>
+               </div>
+               <div class="row">
+                   <div class="col-xs-12 col-sm-6 col-md-3">
+                       <a href="#">
+                           <img src="img/productos/res.jpg" alt="Res" class="img-responsive img-circle">
+                           <h3>Res</h3>
+                       </a>
+                   </div>
+                   <div class="col-xs-12 col-sm-6 col-md-3">
+                       <a href="#">
+                           <img src="img/productos/aves.jpg" alt="Aves" class="img-responsive img-circle">
+                           <h3>Aves</h3>
+                       </a>
+                   </div>
+                   <div class="col-xs-12 col-sm-6 col-md-3">
+                       <a href="#">
+                           <img src="img/productos/cerdo.jpg" alt="Cerdo" class="img-responsive img-circle">
+                           <h3>Cerdo</h3>
+                       </a>
+                   </div>
+                   <div class="col-xs-12 col-sm-6 col-md-3">
+                       <a href="#">
+                           <img src="img/productos/carnes-frias.jpg" alt="Carnes Frias" class="img-responsive img-circle">
+                           <h3>Carnes Frias</h3>
+                       </a>
+                   </div>
+               </div>
+               <div class="row">
+                   <div class="col-xs-12 col-sm-6 col-md-3">
+                       <a href="#">
+                           <img src="img/productos/pescado.jpg" alt="Pescado" class="img-responsive img-circle">
+                           <h3>Pescado</h3>
+                       </a>
+                   </div>
+                   <div class="col-xs-12 col-sm-6 col-md-3">
+                       <a href="#">
+                           <img src="img/productos/lacteos.jpg" alt="Lácteos" class="img-responsive img-circle">
+                           <h3>Lácteos</h3>
+                       </a>
+                   </div>
+                   <div class="col-xs-12 col-sm-6 col-md-3">
+                       <a href="#">
+                           <img src="img/productos/abarrotes.jpg" alt="Abarrotes" class="img-responsive img-circle">
+                           <h3>Abarrotes</h3>
+                       </a>
+                   </div>
+                   <div class="col-xs-12 col-sm-6 col-md-3">
+                       <a href="#">
+                           <img src="img/productos/congelados.jpg" alt="Congelados" class="img-responsive img-circle">
+                           <h3>Congelados</h3>
+                       </a>
+                   </div>
+               </div>
+               <div class="row">
+                   <div class="col-xs-12 col-sm-6 col-md-3">
+                       <a href="#">
+                           <img src="img/productos/cremas-yogurts.jpg" alt="Cremas y Yogurts" class="img-responsive img-circle">
+                           <h3>Cremas Y Yogurts</h3>
+                       </a>
+                   </div>
+                   <div class="col-xs-12 col-sm-6 col-md-3">
+                       <a href="#">
+                           <img src="img/productos/comidas-preparadas.jpg" alt="Comidas Preparadas" class="img-responsive img-circle">
+                           <h3>Comidas Preparadas</h3>
+                       </a>
+                   </div>
+                   <div class="col-xs-12 col-sm-6 col-md-3">
+                       <a href="#">
+                           <img src="img/productos/bebidas.jpg" alt="Bebidas" class="img-responsive img-circle">
+                           <h3>bebidas</h3>
+                       </a>
+                   </div>
+                   <div class="col-xs-12 col-sm-6 col-md-3">
+                       <a href="#">
+                           <img src="img/productos/maduros.jpg" alt="Maduros" class="img-responsive img-circle">
+                           <h3>Maduros</h3>
+                       </a>
+                   </div>
+               </div>
+              </div>
+           </div>
+<?php include('modulos/footer.php'); ?>
---
+++ web/modulos/header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="keywords" content="">
+    
+    <title>CarneMart.com</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Fonts -->
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-carnemart-navbar-collapse-1">
+                     <span class="sr-only">Toggle navigation</span>
+                     <span class="icon-bar"></span>
+                     <span class="icon-bar"></span>
+                     <span class="icon-bar"></span>
+                    </button>
+                    <a href="#" class="navbar-brand"><img src="img/logo.png" alt="CarneMart logo" class="img-responsive"></a>
+                </div>
+                <div class="collapse navbar-collapse navbar-right" id="bs-carnemart-navbar-collapse-1">
+                  <ul class="nav navbar-nav">
+                      <li><a href="#">Inicio</a></li>
+                      <li><a href="#">Nosotros</a></li>
+                      <li class="dropdown">
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Productos <span class="caret"></span></a>
+                          <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Cerdo</a></li>
+                            <li><a href="#">Ave</a></li>
+                            <li><a href="#">Res</a></li>
+                          </ul>
+                        </li>
+                      <li><a href="#">Tiendas</a></li>
+                      <li><a href="#">Promociones</a></li>
+                      <li><a href="#">Contacto</a></li>
+                  </ul>
+          </div>
+            </div>
+        </nav>
+    </header>
